f1d469a0b6dda588f62fe683ed0d6d750554be40
Валерий Орлов <valvic101@mail.ru>
2024-11-29 12:13:24 +0300

    Message handlers are done

---
+++ module_13_2.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher, executor, types
+import asyncio
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+api = os.getenv('BOT_TOKEN')
+bot = Bot(token=api)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот, помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_messages(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
