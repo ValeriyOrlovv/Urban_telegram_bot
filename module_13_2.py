1cd5ceab1a7ec6726cddbcba855d4de22cc741f3
Валерий Орлов <valvic101@mail.ru>
2024-11-29 18:46:43 +0300

    Machine state is done

--- module_13_2.py
+++ module_13_2.py
@@ -1,4 +1,7 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 import asyncio
 from dotenv import load_dotenv
 import os
@@ -7,7 +10,43 @@ import os
 load_dotenv()
 api = os.getenv('BOT_TOKEN')
 bot = Bot(token=api)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    age = State()
+    wieght = State()
+    growth = State()
+
+
+@dp.message_handler(text='Calories')
+async def set_age(message):
+    await message.answer('Введите свой возраст')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=int(message.text))
+    await message.answer('Введите свой рост')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=int(message.text))
+    await message.answer('Введите свой вес')
+    await UserState.wieght.set()
+
+
+@dp.message_handler(state=UserState.wieght)
+async def send_calories(message, state):
+    await state.update_data(weight=int(message.text))
+    data = await state.get_data()
+    calories_limit = 10 * data['weight'] + 6.25 * data['growth'] - 5 * data['age'] + 5
+    await message.answer(f'Ваша норма калорий {calories_limit}')
+    await state.finish()
+
 
 @dp.message_handler(commands=['start'])
 async def start(message):
