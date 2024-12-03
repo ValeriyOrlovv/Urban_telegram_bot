a4a87f85b4d5ef6cfd9254b00cd618db5903fbf9
Валерий Орлов <valvic101@mail.ru>
2024-12-03 15:38:14 +0300

    Keyboard HW

--- module_13_2.py
+++ module_13_2.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import asyncio
 from dotenv import load_dotenv
 import os
@@ -12,14 +13,20 @@ api = os.getenv('BOT_TOKEN')
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+kb.add(button1)
+kb.add(button2)
+
 
 class UserState(StatesGroup):
     age = State()
-    wieght = State()
+    weight = State()
     growth = State()
 
 
-@dp.message_handler(text='Calories')
+@dp.message_handler(text='Рассчитать')
 async def set_age(message):
     await message.answer('Введите свой возраст')
     await UserState.age.set()
@@ -27,30 +34,43 @@ async def set_age(message):
 
 @dp.message_handler(state=UserState.age)
 async def set_growth(message, state):
-    await state.update_data(age=int(message.text))
-    await message.answer('Введите свой рост')
-    await UserState.growth.set()
+    try:
+        await state.update_data(age=int(message.text))
+        await message.answer('Введите свой рост')
+        await UserState.growth.set()
+    except ValueError:
+        await message.answer('Введите, пожалуйста, число')
+   
 
 
 @dp.message_handler(state=UserState.growth)
 async def set_weight(message, state):
-    await state.update_data(growth=int(message.text))
-    await message.answer('Введите свой вес')
-    await UserState.wieght.set()
+    try:
+        await state.update_data(growth=int(message.text))
+        await message.answer('Введите свой вес')
+        await UserState.weight.set()
+    except ValueError:
+        await message.answer('Введите, пожалуйста, число')
 
 
-@dp.message_handler(state=UserState.wieght)
+@dp.message_handler(state=UserState.weight)
 async def send_calories(message, state):
-    await state.update_data(weight=int(message.text))
-    data = await state.get_data()
-    calories_limit = 10 * data['weight'] + 6.25 * data['growth'] - 5 * data['age'] + 5
-    await message.answer(f'Ваша норма калорий {calories_limit}')
-    await state.finish()
+    try:
+        await state.update_data(weight=int(message.text))
+        data = await state.get_data()
+        calories_limit = 10 * data['weight'] + 6.25 * data['growth'] - 5 * data['age'] + 5
+        await message.answer(f'Ваша норма калорий {calories_limit}')
+        await state.finish()
+    except ValueError:
+        await message.answer('Введите, пожалуйста, число')
 
 
 @dp.message_handler(commands=['start'])
 async def start(message):
-    await message.answer('Привет! Я бот, помогающий твоему здоровью.')
+    await message.answer(
+        'Привет! Я бот, помогающий твоему здоровью.',
+        reply_markup=kb
+    )
 
 
 @dp.message_handler()
